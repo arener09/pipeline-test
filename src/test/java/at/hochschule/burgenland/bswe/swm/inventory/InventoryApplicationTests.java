db36466a6f5be23edc8094915f2f07bd4e1d7912
Alexander R. Brenner <alexanderreinhard.brenner@ikt4u.com>
2025-10-06 16:32:24 +0200

    feat: test

---
+++ src/test/java/at/hochschule/burgenland/bswe/swm/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package at.hochschule.burgenland.bswe.swm.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
